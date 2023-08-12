1aeb127f25ea8ce5cebfad238e4a8803275a432a
Andrey Miskov <amiskov@gmail.com>
2023-08-12 16:03:14 +0300

    publish photo to Telegram channel

--- tg_bot.py
+++ tg_bot.py
@@ -1,14 +1,23 @@
+import os
+import random
+
 import telegram
 from environs import Env
+
+
+def get_random_filename(dir: str) -> str:
+    filename = random.choice(os.listdir(dir))
+    return f'{dir}/{filename}'
 
 
 def main():
     env = Env()
     env.read_env()
     TELEGRAM_API_TOKEN = env('TELEGRAM_API_TOKEN')
+    TELEGRAM_CHAT_ID = env('TELEGRAM_CHAT_ID')
     bot = telegram.Bot(token=TELEGRAM_API_TOKEN)
-    print(bot.get_me())
-    bot.send_message(text='Hello from tb_bot.py!', chat_id='@andreysSpacePics')
+    bot.send_document(chat_id=TELEGRAM_CHAT_ID,
+                      document=open(get_random_filename('images'), 'rb'))
 
 
 if __name__ == '__main__':
