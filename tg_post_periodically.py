7d6242abb9a86a546bcca63ec944737e3d95764e
Andrey Miskov <amiskov@gmail.com>
2023-08-16 05:42:04 +0300

    make filepath crossplatform

--- tg_post_periodically.py
+++ tg_post_periodically.py
@@ -32,8 +32,8 @@ def main():
         send_files_periodically(bot, TELEGRAM_CHAT_ID, shuffled_pics, period)
 
 
-def send_files_periodically(bot: telegram.Bot, chat_id: str,
-                            files: list[str], period: timedelta):
+def send_files_periodically(bot: telegram.Bot, chat_id: str, files: list[str],
+                            period: timedelta):
     """Send each file from `files` via `bot` to `chat_id` with `period`.
 
     Removes a file path from `files` after sending. Stops when `files`
@@ -43,8 +43,7 @@ def send_files_periodically(bot: telegram.Bot, chat_id: str,
     logging.info(f'Start sending {files_count} files.')
     while len(files) > 0:
         file = files.pop()
-        bot.send_document(chat_id=chat_id,
-                          document=open(f'images/{file}', 'rb'))
+        bot.send_document(chat_id=chat_id, document=open(file, 'rb'))
         logging.info(f'{file} sent, {len(files)} files remains.')
         time.sleep(period.total_seconds())
     logging.info(f'{files_count} are sent.')
